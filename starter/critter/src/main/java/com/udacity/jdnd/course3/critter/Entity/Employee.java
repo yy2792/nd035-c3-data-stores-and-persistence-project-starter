8012b48339a375e5f4eed9457f0488bfba5f062e
Yingke Yu <yy2792@columbia.edu>
2022-12-02 14:39:42 -0500

    Update Employee.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/Entity/Employee.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/Entity/Employee.java
@@ -22,6 +22,6 @@ public class Employee {
     @ElementCollection
     @Enumerated(EnumType.STRING)
     private Set<DayOfWeek> daysAvailable;
-    @ManyToMany(mappedBy="employees")
-    private Set<Schedule> schedules;
+    @ManyToMany(mappedBy="employeeList")
+    private Set<Schedule> employeeSchedules;
 }
