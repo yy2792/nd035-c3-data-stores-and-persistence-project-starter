9e5953cde5946747fa0869e7cd19a9e710a38571
Yingke Yu <yy2792@columbia.edu>
2022-12-02 16:26:56 -0500

    Update CustomerService.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
@@ -1,11 +1,17 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import com.udacity.jdnd.course3.critter.Entity.Customer;
+import com.udacity.jdnd.course3.critter.Entity.Pet;
+import com.udacity.jdnd.course3.critter.Error.PetNotFoundError;
 import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
 import com.udacity.jdnd.course3.critter.repository.PetRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Transactional
 @Service
@@ -19,6 +25,36 @@ public class CustomerService {
         this.petRepository = repository;
         this.customerRepository = customerRepository;
     }
+
+    public Customer saveCustomer(Customer customer) {
+        customer = customerRepository.save(customer);
+        return customer;
+    }
+
+    private CustomerDTO convertEntityToCustomerDTO(Customer customer) {
+        CustomerDTO customerDTO = new CustomerDTO();
+        BeanUtils.copyProperties(customer, customerDTO);
+        if(customer.getPets() != null){
+            List<Long> petIds = new ArrayList<>();
+            customer.getPets().forEach(pet -> petIds.add(pet.getId()));
+            customerDTO.setPetIds(petIds);
+        }
+        return customerDTO;
+    }
+
+    private Customer convertCustomerDTOToEntity(CustomerDTO customerDTO) throws PetNotFoundError{
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDTO, customer);
+        if(customerDTO.getPetIds() != null){
+            List<Pet> pets = new ArrayList<>();
+            customerDTO.getPetIds().forEach(
+                    petId -> pets.add(petRepository.findById(petId).orElseThrow(() -> new PetNotFoundError("Pet " + String.valueOf(petId) + "not found!"))));
+            customer.setPets(pets);
+        }
+        return customer;
+    }
+
+
 
 
 
