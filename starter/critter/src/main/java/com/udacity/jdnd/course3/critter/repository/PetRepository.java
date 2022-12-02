866e8e3a7801d8b5d8610c05d8e9f43fe59b3864
Yingke Yu <yy2792@columbia.edu>
2022-12-02 13:56:16 -0500

    Update PetRepository.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/PetRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/PetRepository.java
@@ -3,9 +3,11 @@ package com.udacity.jdnd.course3.critter.repository;
 import com.udacity.jdnd.course3.critter.model.Customer;
 import com.udacity.jdnd.course3.critter.model.Pet;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PetRepository extends CrudRepository<Pet, Long> {
     List<Pet> getPetsByCustomer(Customer customer);
 }
