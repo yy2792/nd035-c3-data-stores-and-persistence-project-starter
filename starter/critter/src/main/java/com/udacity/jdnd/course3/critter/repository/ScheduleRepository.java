1261f7a29e2bd604a219baee5c7ea20c6ebd7fc3
Yingke Yu <yy2792@columbia.edu>
2022-12-02 13:59:38 -0500

    Create ScheduleRepository.java

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -0,0 +1,8 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
+
+}
