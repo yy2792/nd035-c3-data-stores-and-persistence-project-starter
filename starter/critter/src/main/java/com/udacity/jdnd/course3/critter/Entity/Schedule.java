1c79a9bc137a519d2b8f08cc8be035b56d8ef28e
Yingke Yu <yy2792@columbia.edu>
2022-12-02 14:39:47 -0500

    Update Schedule.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/Entity/Schedule.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/Entity/Schedule.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Entity
@@ -14,5 +15,16 @@ public class Schedule {
     private Long id;
     private LocalDate date;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "employee_schedule",
+    joinColumns = @JoinColumn(name = "schedule_id"),
+    inverseJoinColumns = @JoinColumn(name = "employee_id"))
+    private List<Employee> employeeList;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "pet_schedule",
+    joinColumns = @JoinColumn(name = "schedule_id"),
+    inverseJoinColumns = @JoinColumn(name = "pet_id"))
+    private List<Pet> petList;
 
 }
