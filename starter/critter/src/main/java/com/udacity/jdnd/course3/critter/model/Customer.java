86c28e7d1e5bdb3afe9c341393895609e81da94e
Yingke Yu <yy2792@columbia.edu>
2022-12-02 13:39:58 -0500

    Create Customer.java

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/model/Customer.java
@@ -0,0 +1,23 @@
+package com.udacity.jdnd.course3.critter.model;
+
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String phone;
+    @Nationalized
+    private String name;
+    @Nationalized
+    private String notes;
+    @OneToMany(targetEntity = Pet.class, mappedBy = "owner", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Pet> pets;
+}
