f3a5e776f7e03c33680993a4ca4e06ef6892c7af
Yingke Yu <yy2792@columbia.edu>
2022-12-02 14:39:49 -0500

    Update ScheduleRepository.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -1,5 +1,7 @@
 package com.udacity.jdnd.course3.critter.repository;
 
+import com.udacity.jdnd.course3.critter.Entity.Schedule;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
