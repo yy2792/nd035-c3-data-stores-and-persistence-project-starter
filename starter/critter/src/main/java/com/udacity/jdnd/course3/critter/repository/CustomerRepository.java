b70b389236a7d2197fa3339e29e5b422b584a6a4
Yingke Yu <yy2792@columbia.edu>
2022-12-02 13:57:27 -0500

    Create CustomerRepository.java

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.udacity.jdnd.course3.critter.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+}
