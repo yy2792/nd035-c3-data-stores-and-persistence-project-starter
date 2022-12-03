c50b0dee046c4dd5d578af7a7cd72a44e41661a9
Yingke Yu <yy2792@columbia.edu>
2022-12-02 21:28:28 -0500

    Update PetService.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.udacity.jdnd.course3.critter.Entity.Customer;
 import com.udacity.jdnd.course3.critter.Entity.Pet;
 import com.udacity.jdnd.course3.critter.Error.CustomerNotFoundError;
+import com.udacity.jdnd.course3.critter.Error.PetNotFoundError;
 import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
 import com.udacity.jdnd.course3.critter.repository.PetRepository;
 import org.springframework.beans.BeanUtils;
@@ -31,6 +32,11 @@ public class PetService {
     public Pet savePet(Pet pet) {
         pet = petRepository.save(pet);
         return pet;
+    }
+
+    public Pet getPetById(Long id) {
+        return petRepository.findById(id).orElseThrow(() ->
+                new PetNotFoundError("Pet " + id.toString() + " not found"));
     }
 
     public PetDTO convertEntityToPetDTO(Pet pet) {
