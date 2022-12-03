1cb35eaaf36671b1c421dc4ba3971e2d6834d7ae
Yingke Yu <yy2792@columbia.edu>
2022-12-03 18:30:41 -0500

    Update ScheduleRepository.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -1,10 +1,16 @@
 package com.udacity.jdnd.course3.critter.repository;
 
+import com.udacity.jdnd.course3.critter.Entity.Employee;
+import com.udacity.jdnd.course3.critter.Entity.Pet;
 import com.udacity.jdnd.course3.critter.Entity.Schedule;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
-
+    List<Schedule> findAllByPetsIn(List<Pet> pets);
+    List<Schedule> findAllByEmployeesContaining(Employee employee);
+    List<Schedule> findAllByPetsContaining(Pet pet);
 }
