1898aa8b0237f2bfeb58b516d1c6d404088c3f60
Yingke Yu <yy2792@columbia.edu>
2022-12-05 11:31:32 -0500

    Update EmployeeRepository.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
@@ -4,7 +4,10 @@ import com.udacity.jdnd.course3.critter.Entity.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.DayOfWeek;
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
+    List<Employee> getAllByDaysAvailable(DayOfWeek dayOfWeek);
 }
