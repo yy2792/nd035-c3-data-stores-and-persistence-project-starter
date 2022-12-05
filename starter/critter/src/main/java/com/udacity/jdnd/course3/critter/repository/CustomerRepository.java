aca40d109c580b3e4f50ad016354bb25bcd0713e
Yingke Yu <yy2792@columbia.edu>
2022-12-05 11:46:14 -0500

    Update CustomerRepository.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
@@ -1,8 +1,11 @@
 package com.udacity.jdnd.course3.critter.repository;
 
 import com.udacity.jdnd.course3.critter.Entity.Customer;
+import com.udacity.jdnd.course3.critter.Entity.Pet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
